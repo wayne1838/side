6cbfa5cf1932ba7a7cbb4ca4b4bb5eebeee63c7e
wayne1838 <wayne61006@gmail.com>
2016-01-31 19:52:57 +0800

    2016/01/25	add send massage 增加功能 簡易切換畫面 簡易對話功能 串接使用firebase
    參考資料
    firebase開發
    https://cloud.google.com/solutions/mobile/firebase-app-engine-android-studio
    
    新增activity 頁面
    http://lupin33androidapp.blogspot.tw/2015/01/android-new-activity.html
    
    wayne1838

--- app/src/main/java/com/example/wayne1838/side/MainActivity.java
+++ app/src/main/java/com/example/wayne1838/side/MainActivity.java
@@ -1,13 +1,46 @@
 package com.example.wayne1838.side;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //super引用父類別
         super.onCreate(savedInstanceState);
+        //設定使用的view
         setContentView(R.layout.activity_main);
+        //設定button01 為Button元件  利用id在view中選找某元件
+        Button button01 = (Button)findViewById(R.id.btnEnter);
+        //為button01設定監聽 new一個按鈕按下後 將啟用的方法
+        button01.setOnClickListener(new Button.OnClickListener() {
+
+            public void onClick(View v) {
+
+                // TODO Auto-generated method stub
+                //設置一個intent來開啟另一個activity
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, TalkMain.class);
+                startActivity(intent);
+
+                //關閉這個activity
+                MainActivity.this.finish();
+
+            }
+
+        });
+
     }
+
 }
---
+++ app/src/main/java/com/example/wayne1838/side/TalkMain.java
@@ -0,0 +1,103 @@
+package com.example.wayne1838.side;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+/**
+ * Created by wayne1838 on 16/1/29.
+ */
+public class TalkMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //super引用父類別
+        super.onCreate(savedInstanceState);
+        //設定使用的view
+        setContentView(R.layout.talk_main);
+
+        // Get ListView object from xml
+        final ListView listView = (ListView) findViewById(R.id.listView);
+
+        // Create a new Adapter
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1);
+
+        // Assign adapter to ListView
+        listView.setAdapter(adapter);
+
+        // Use Firebase to populate the list.
+        Firebase.setAndroidContext(this);
+
+        new Firebase("https://brilliant-fire-2936.firebaseio.com/todoItems")
+                .addChildEventListener(new ChildEventListener() {
+                    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                        adapter.add((String) dataSnapshot.child("text").getValue());
+                    }
+
+                    public void onChildRemoved(DataSnapshot dataSnapshot) {
+                        adapter.remove((String) dataSnapshot.child("text").getValue());
+                    }
+
+                    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                    }
+
+                    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+                    }
+
+                    public void onCancelled(FirebaseError firebaseError) {
+                    }
+                });
+
+        // Add items via the Button and EditText at the bottom of the window.
+        final EditText text = (EditText) findViewById(R.id.todoText);
+        final Button button = (Button) findViewById(R.id.addButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+
+                new Firebase("https://brilliant-fire-2936.firebaseio.com/todoItems")
+                        .push()
+                        .child("text")
+                        .setValue(text.getText().toString());
+
+                text.setText("");
+            }
+        });
+
+        // Delete items when clicked
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                new Firebase("https://brilliant-fire-2936.firebaseio.com/todoItems")
+                        .orderByChild("text")
+                        .equalTo((String) listView.getItemAtPosition(position))
+                        .addListenerForSingleValueEvent(new ValueEventListener() {
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+                                if (dataSnapshot.hasChildren()) {
+                                    DataSnapshot firstChild = dataSnapshot.getChildren().iterator().next();
+                                    firstChild.getRef().removeValue();
+                                }
+                            }
+
+                            public void onCancelled(FirebaseError firebaseError) {
+                            }
+                        });
+            }
+        });
+    }
+
+}
